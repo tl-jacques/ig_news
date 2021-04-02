ab5426b1d08883a624971539d35bc567b8598153
thiago <thiago_linhares_jacques@hotmail.com>
2021-04-02 13:33:44 -0300

    feat: component Header and SignInButton done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,17 @@
-export function Header () {
-    return (
-        <header>
-            <div></div>
-        </header>
-    )
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="Ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
 }
