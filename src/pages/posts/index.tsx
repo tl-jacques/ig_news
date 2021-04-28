ed27f523ae8f669935061eccd43269aafc7bd65b
thiago <thiago_linhares_jacques@hotmail.com>
2021-04-28 16:16:26 -0300

    feature: posts page fetching data from prismic api

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,6 +1,23 @@
 import Head from "next/head";
+import { GetStaticProps } from "next";
+
 import styles from "./styles.module.scss";
-export default function Posts() {
+
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
+import { getPrismicClient } from "../../services/prismic";
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+interface PostProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -8,50 +25,41 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="dakjsdka">
-            <time>27 de abril de 2021</time>
-            <strong>
-              How To Deal With Annoying Neighbors Without Creating Enemies
-            </strong>
-            <p>
-              I’ve lived in my current house for more than 10 years now. My
-              neighborhood is in a prime location in South Tampa. The real
-              estate value has been going up rapidly. Consequently, people are
-              coming and going, moving in and out all the time. It’s made for an
-              interesting experience — and a couple of annoying neighbors along
-              the way.
-            </p>
-          </a>
-          <a href="dakjsdka">
-            <time>27 de abril de 2021</time>
-            <strong>
-              How To Deal With Annoying Neighbors Without Creating Enemies
-            </strong>
-            <p>
-              I’ve lived in my current house for more than 10 years now. My
-              neighborhood is in a prime location in South Tampa. The real
-              estate value has been going up rapidly. Consequently, people are
-              coming and going, moving in and out all the time. It’s made for an
-              interesting experience — and a couple of annoying neighbors along
-              the way.
-            </p>
-          </a>
-          <a href="dakjsdka">
-            <time>27 de abril de 2021</time>
-            <strong>
-              How To Deal With Annoying Neighbors Without Creating Enemies
-            </strong>
-            <p>
-              I’ve lived in my current house for more than 10 years now. My
-              neighborhood is in a prime location in South Tampa. The real
-              estate value has been going up rapidly. Consequently, people are
-              coming and going, moving in and out all the time. It’s made for an
-              interesting experience — and a couple of annoying neighbors along
-              the way.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="dakjsdka">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title} </strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "publication")],
+    { fetch: ["publication.title", "publication.content"], pageSize: 100 }
+  );
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+  return { props: { posts } };
+};
